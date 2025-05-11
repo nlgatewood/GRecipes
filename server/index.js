fd41dd8ad80e7c9297fb2278d25fdad9f8dc1b3f
nlgatewood <nlgatewood@localhost.localdomain>
2025-05-11 15:34:47 -0400

    Added generic postgres db functionality for pulling data

--- server/index.js
+++ server/index.js
@@ -1,20 +1,63 @@
+const { Client } = require('pg');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
 app.use(cors())
 
+const dbConfig = {
+  user: 'nlgatewood',
+  password: '',
+  host: '192.168.1.14',
+  port: 5432,
+  database: 'nlgatewood',
+};
+
+//Generic GET Request
 app.get('/', (req, res) => {
 
       res.send('Hello from our server!')
 })
 
+//datetime GET Request
 app.get('/datetime', (req, res) => {
-      
-      res.send(new Date())
+
+      // Create a new PostgreSQL client
+      const client = new Client(dbConfig);
+
+      client
+	      .connect()
+	      .then(() => {
+		      console.log('Connected to PostgreSQL database');
+
+		      // Execute SQL queries here
+
+		      client.query('SELECT NOW()', (err, result) => {
+
+			      if (err) {
+				      console.error('Error executing query', err);
+			      } else {
+                              res.send(result.rows[0]['now'])
+				      //console.log('Query result:', result.rows[0]['now']);
+			      }
+
+			      // Close the connection when done
+			      client
+				      .end()
+				      .then(() => {
+					      console.log('Connection to PostgreSQL closed');
+				      })
+				      .catch((err) => {
+					      console.error('Error closing connection', err);
+				      });
+		      });
+	      })
+	      .catch((err) => {
+		      console.error('Error connecting to PostgreSQL database', err);
+	      });
 })
 
 app.listen(8080, () => {
-      
+
       console.log('server listening on port 8080')
 })
